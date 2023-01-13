c79c8ff82b6fb6af7fe8d86e94bdaeeb7bc67b00
José Luis González <joseluisgonsan@gmail.com>
2023-01-13 18:31:17 +0100

    Pretty Print

--- src/main/kotlin/joseluisgs/es/plugins/Serialization.kt
+++ src/main/kotlin/joseluisgs/es/plugins/Serialization.kt
@@ -1,20 +1,16 @@
 package joseluisgs.es.plugins
 
 import io.ktor.serialization.kotlinx.json.*
-import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
-import io.ktor.server.routing.*
+import io.ktor.server.plugins.contentnegotiation.*
+import kotlinx.serialization.json.Json
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
-        json()
-    }
-
-    routing {
-        get("/json/kotlinx-serialization") {
-            call.respond(mapOf("hello" to "world"))
-        }
+        // Lo ponemos bonito :)
+        json(Json {
+            prettyPrint = true
+            isLenient = true
+        })
     }
 }
