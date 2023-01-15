a9dfa55338941064190a6a5a04cb4675e6cc879e
José Luis González <joseluisgonsan@gmail.com>
2023-01-15 14:58:07 +0100

    Ajustes Dtos

--- src/main/kotlin/joseluisgs/es/dto/Representantes.kt
+++ src/main/kotlin/joseluisgs/es/dto/Representantes.kt
@@ -1,21 +1,33 @@
 package joseluisgs.es.dto
 
-import joseluisgs.es.models.Representante
 import joseluisgs.es.serializers.LocalDateTimeSerializer
+import joseluisgs.es.serializers.UUIDSerializer
 import kotlinx.serialization.Serializable
 import java.time.LocalDateTime
+import java.util.*
 
 @Serializable
 data class RepresentantesPageDTO(
     val page: Int,
     val perPage: Int,
-    val data: List<Representante>,
+    val data: List<RepresentanteDTO>,
     @Serializable(with = LocalDateTimeSerializer::class)
     val createdAt: LocalDateTime? = LocalDateTime.now()
 )
 
 @Serializable
 data class RepresentanteDTO(
+    @Serializable(with = UUIDSerializer::class)
+    val id: UUID? = null,
     val nombre: String,
-    val email: String
-)
+    val email: String,
+    val metadata: MetaData? = null,
+) {
+    @Serializable
+    class MetaData(
+        @Serializable(with = LocalDateTimeSerializer::class)
+        val createdAt: LocalDateTime? = LocalDateTime.now(),
+        @Serializable(with = LocalDateTimeSerializer::class)
+        val updatedAt: LocalDateTime? = LocalDateTime.now()
+    )
+}
--- src/main/kotlin/joseluisgs/es/mappers/Representantes.kt
+++ src/main/kotlin/joseluisgs/es/mappers/Representantes.kt
@@ -4,8 +4,13 @@ import joseluisgs.es.dto.RepresentanteDTO
 import joseluisgs.es.models.Representante
 
 fun Representante.toDto() = RepresentanteDTO(
+    id = this.id,
     nombre = this.nombre,
-    email = this.email
+    email = this.email,
+    metadata = RepresentanteDTO.MetaData(
+        createdAt = this.createdAt,
+        updatedAt = this.updatedAt
+    )
 )
 
 fun RepresentanteDTO.toModel() = Representante(
