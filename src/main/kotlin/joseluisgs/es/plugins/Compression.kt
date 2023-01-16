50545214ea72099e134f631ed7e04f35dde0506d
José Luis González <joseluisgonsan@gmail.com>
2023-01-16 18:04:52 +0100

    Cpomprensión minimo 1024

--- src/main/kotlin/joseluisgs/es/plugins/Compression.kt
+++ src/main/kotlin/joseluisgs/es/plugins/Compression.kt
@@ -8,8 +8,9 @@ fun Application.configureCompression() {
     // Definimos una estrategia de compresión de contenido
     install(Compression) {
         gzip {
-            // El tamaño mínimo para empezar a comprimir
-            // minimumSize(1024)
+            // El tamaño mínimo para empezar a comprimir, podemos fijar un tamaño en bytes
+            // y otras opciones
+            minimumSize(1024)
         }
     }
 }
