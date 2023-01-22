53028ad98083d45013c17c72645229c30b192b54
José Luis González <joseluisgonsan@gmail.com>
2023-01-22 19:50:16 +0100

    Raquetas Validator

--- src/main/kotlin/joseluisgs/es/plugins/Validation.kt
+++ src/main/kotlin/joseluisgs/es/plugins/Validation.kt
@@ -2,6 +2,7 @@ package joseluisgs.es.plugins
 
 import io.ktor.server.application.*
 import io.ktor.server.plugins.requestvalidation.*
+import joseluisgs.es.validators.raquetasValidation
 import joseluisgs.es.validators.representantesValidation
 import joseluisgs.es.validators.usersValidation
 
@@ -9,5 +10,6 @@ fun Application.configureValidation() {
     install(RequestValidation) {
         usersValidation()
         representantesValidation()
+        raquetasValidation()
     }
 }
