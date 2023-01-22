e6f00571ae29a9e2c6ade69661ed7c29114a677f
José Luis González <joseluisgonsan@gmail.com>
2023-01-22 12:25:10 +0100

    User Entity

--- src/main/kotlin/joseluisgs/es/mappers/Users.kt
+++ src/main/kotlin/joseluisgs/es/mappers/Users.kt
@@ -2,6 +2,7 @@ package joseluisgs.es.mappers
 
 import joseluisgs.es.dto.UserCreateDto
 import joseluisgs.es.dto.UserDto
+import joseluisgs.es.entities.UserEntity
 import joseluisgs.es.models.User
 
 fun User.toDto(): UserDto {
@@ -30,6 +31,37 @@ fun UserCreateDto.toModel(): User {
         role = this.role
     )
 }
+
+fun UserEntity.toModel(): User {
+    return User(
+        id = this.id,
+        nombre = this.nombre,
+        email = this.email,
+        username = this.username,
+        password = this.password,
+        avatar = this.avatar,
+        role = User.Role.valueOf(this.role),
+        createdAt = this.createdAt,
+        updatedAt = this.updatedAt,
+        deleted = this.deleted
+    )
+}
+
+fun User.toEntity(): UserEntity {
+    return UserEntity(
+        id = this.id,
+        nombre = this.nombre,
+        email = this.email,
+        username = this.username,
+        password = this.password,
+        avatar = this.avatar,
+        role = this.role.name,
+        createdAt = this.createdAt,
+        updatedAt = this.updatedAt,
+        deleted = this.deleted
+    )
+}
+
 
 
 
