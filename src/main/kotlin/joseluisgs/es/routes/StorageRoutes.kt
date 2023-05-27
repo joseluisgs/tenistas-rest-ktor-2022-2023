1d1fd91206214225cd4904be8392207785f1228c
José Luis González <joseluisgonsan@gmail.com>
2023-05-27 12:19:27 +0200

    Storage con Results

--- src/main/kotlin/joseluisgs/es/routes/StorageRoutes.kt
+++ src/main/kotlin/joseluisgs/es/routes/StorageRoutes.kt
@@ -1,17 +1,20 @@
 package joseluisgs.es.routes
 
+import com.github.michaelbull.result.mapBoth
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
+import io.ktor.server.plugins.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.util.pipeline.*
+import joseluisgs.es.errors.StorageError
 import joseluisgs.es.services.storage.StorageService
 import mu.KotlinLogging
 import org.koin.ktor.ext.inject
 import java.time.LocalDateTime
-import java.util.*
 
 private val logger = KotlinLogging.logger {}
 
@@ -38,14 +41,18 @@ fun Application.storageRoutes() {
                 )
             }
             post {
+                val baseUrl =
+                    call.request.origin.scheme + "://" + call.request.host() + ":" + call.request.port() + "/$ENDPOINT/"
                 // Recibimos el archivo
                 logger.debug { "POST /$ENDPOINT" }
                 val readChannel = call.receiveChannel()
                 // Lo guardamos en disco
-                val fileName = UUID.randomUUID().toString()
-                val res = storageService.saveFile(fileName, readChannel)
-                // Respondemos
-                call.respond(HttpStatusCode.OK, res)
+                val newFileName = System.currentTimeMillis().toString()
+                val newFileUrl = "$baseUrl$newFileName"
+                storageService.saveFile(newFileName, newFileUrl, readChannel).mapBoth(
+                    success = { call.respond(HttpStatusCode.Created, it) },
+                    failure = { handleStorageErrors(it) }
+                )
             }
 
             // GET -> /{fileName}
@@ -55,14 +62,17 @@ fun Application.storageRoutes() {
                 // Recuperamos el nombre del fichero
                 val fileName = call.parameters["fileName"].toString()
                 // Recuperamos el fichero
-                val file = storageService.getFile(fileName)
+                val file = storageService.getFile(fileName).mapBoth(
+                    success = { call.respondFile(it) },
+                    failure = { handleStorageErrors(it) }
+                )
                 // De esta manera lo podria visiualizar el navegador
                 // call.respondFile(file)
                 // si lo hago así me pide descargar
                 //call.response.header("Content-Disposition", "attachment; filename=\"${file.name}\"")
                 // Para hacer lo anterior lo mejor es saber si tiene una extensión
                 // y en función de eso devolver un tipo de contenido
-                call.respondFile(file)
+
 
             }
 
@@ -78,11 +88,21 @@ fun Application.storageRoutes() {
                     // Recuperamos el nombre del fichero
                     val fileName = call.parameters["fileName"].toString()
                     // Recuperamos el fichero
-                    storageService.deleteFile(fileName)
-                    // Respondemos
-                    call.respond(HttpStatusCode.NoContent)
+                    storageService.deleteFile(fileName).mapBoth(
+                        success = { call.respond(HttpStatusCode.NoContent) },
+                        failure = { handleStorageErrors(it) }
+                    )
                 }
             }
         }
+    }
+}
+
+private suspend fun PipelineContext<Unit, ApplicationCall>.handleStorageErrors(
+    error: StorageError,
+) {
+    when (error) {
+        is StorageError.FileNotFound -> call.respond(HttpStatusCode.NotFound, error.message)
+        is StorageError.FileNotSave -> call.respond(HttpStatusCode.BadRequest, error.message)
     }
 }
